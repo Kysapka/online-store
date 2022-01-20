d9b5453ae1d8532f56785ed147f7fc04139492ba
Артем <kysapka@gmail.com>
2022-01-21 01:41:52 +0300

    fix start

--- db.js
+++ db.js
@@ -1,13 +1,14 @@
 const { Sequelize } = require('sequelize')
+const {ssl} = require("pg/lib/defaults");
 
 module.exports = new Sequelize(
     process.env.DB_NAME, // Название БД
     process.env.DB_USER, // Пользователь
     process.env.DB_PASSWORD, // Пароль
+    ssl,
     {
         dialect: 'postgres',
         host: process.env.DB_HOST,
         port: process.env.DB_PORT,
-        ssl: true
     }
 )
